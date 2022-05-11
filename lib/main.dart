e03cdc1dc90392faac0a6e3396e98f90261d110f
Adan <adancondoric@gmail.com>
2022-05-11 17:47:44 -0400

    add new interface and add font

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:qradm/screens/Detail.dart';
+import 'package:qradm/screens/GradientBack.dart';
+import 'package:qradm/screens/ListOpinion.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,12 +9,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+  static const String description = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. \n\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +29,21 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        //body: new Detail("Bahamas", 4, description),
+        //body: GradientBack(),//new Detail("Bahamas", 4, description),
+        body: Stack(
+          children: <Widget>[
+            ListView(
+              children: <Widget>[
+                Detail("Bahamas", 4, description),
+                ListOpinion()
+              ],
+            ),
+            GradientBack()
+          ],
+        ),
+      )
     );
   }
 }
---
+++ lib/screens/ListOpinion.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'Opinion.dart';
+
+class ListOpinion extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 1", "1 review · 5 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 2", "2 review · 5 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+        Opinion("lib/assets/icon/icon.png", "Hola mundo 3", "2 review · 2 photos", "There is an amazing place in Sri Lanka"),
+
+      ],
+    );
+  }
+
+}
