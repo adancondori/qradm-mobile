f887f8dd66d072461ffa1446f95bc84f0a802710
adancondori <adancondoric@gmail.com>
2022-05-16 00:30:37 -0400

    refactor details

--- lib/screens/Detail.dart
+++ lib/screens/Detail.dart
@@ -87,8 +87,9 @@ class Detail extends StatelessWidget {
           foregroundColor: MaterialStateProperty.all<Color>(Colors.blue),
           overlayColor: MaterialStateProperty.resolveWith<Color?>(
                 (Set<MaterialState> states) {
-              if (states.contains(MaterialState.hovered))
+              if (states.contains(MaterialState.hovered)) {
                 return Colors.blue.withOpacity(0.04);
+              }
               if (states.contains(MaterialState.focused) ||
                   states.contains(MaterialState.pressed))
                 return Colors.blue.withOpacity(0.12);
