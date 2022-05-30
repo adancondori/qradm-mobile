0e0c49da498af4f66a7f855c368adc9ef43e177a
Sakenita <sakenita@gmail.com>
2022-05-30 16:45:37 -0400

    Addition of libraries for QR scanner

--- lib/src/read_qr/ui/screens/ReadQR.dart
+++ lib/src/read_qr/ui/screens/ReadQR.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'dart:io';
 
 class ReadQR extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
       color: Colors.greenAccent,
     );
   }
 }
+
