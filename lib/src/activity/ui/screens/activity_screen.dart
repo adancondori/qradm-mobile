20d1c7cb9ac4cd720acc6e473732c81a0e3c97f2
Gabriel Santos <gabriel_bodomir@hotmail.com>
2022-05-31 23:26:01 -0400

    Implementation of vector in list activities

--- lib/src/activity/ui/screens/activity_screen.dart
+++ lib/src/activity/ui/screens/activity_screen.dart
@@ -1,8 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:qradm/src/activity/bloc/list_activity.dart';
+import '../widgets/Activity.dart';
 class ActivityScreen extends StatelessWidget {
+  List<Activity> activitys =[];
+
+  void generateListActivity() {
+    activitys = [];
+    for(var i = 0 ; i < 20; i++ ) {
+      var activity = Activity(id: 1,
+          name: "Actividad ${i}",
+          detail: "description de la actividad ${i}");
+      activitys.add(activity);
+    }
+  }
   @override
   Widget build(BuildContext context) {
+    generateListActivity();
     // TODO: implement build
     return Container(
       color: Colors.white,
@@ -12,20 +25,12 @@ class ActivityScreen extends StatelessWidget {
             margin: const EdgeInsets.only(
               top: 50,
             ),
-            child: ListView(
-              children: <Widget>[
-                ListActivity("Actividad 1", "primera actividad"),
-                ListActivity("Actividad 2", "Segunda actividad"),
-                ListActivity("Actividad 3", "tercera actividad"),
-                ListActivity("Actividad 4", "cuarta actividad"),
-                ListActivity("Actividad 5", "quinta actividad"),
-                ListActivity("Actividad 6", "sexta actividad"),
-                ListActivity("Actividad 7", "septima actividad"),
-                ListActivity("Actividad 8", "octava actividad"),
-                ListActivity("Actividad 9", "novena actividad"),
-                ListActivity("Actividad 10", "decima actividad"),
-                ListActivity("Actividad 11", "ondeava actividad"),
-              ],
+            child: ListView.builder(
+                padding: const EdgeInsets.all(8),
+                itemCount: activitys.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListActivity(activitys[index].name, activitys[index].detail);
+                }
             ),
           ),
         ],
