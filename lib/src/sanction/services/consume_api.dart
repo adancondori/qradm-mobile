009b87f8a02d48bf940c3fe03b4102a76ac19768
adancondori <adancondoric@gmail.com>
2022-06-27 23:41:55 -0400

    fix url

--- lib/src/sanction/services/consume_api.dart
+++ lib/src/sanction/services/consume_api.dart
@@ -1,12 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:qradm/src/sanction/model/sanction_api.dart';
+import 'package:qradm/src/service/app_constant.dart';
 
 class SanctionService {
   Future<List<Sanctions>?> getSanctions() async {
     var client = http.Client();
 
-    var uri = Uri.parse('https://www.eventsadm.space/api/v1/mobile/sanctions');
+    var uri = Uri.parse(AppConstants.BASE_URL + '/sanctions');
     var response = await client.get(uri);
     if (response.statusCode == 200) {
       var json1 = json.decode(response.body);
@@ -15,5 +16,3 @@ class SanctionService {
     }
   }
 }
-
-
